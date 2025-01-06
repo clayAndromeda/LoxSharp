d518f4dc8c34143e9b8cb1b07ab68475fc9573e3
clayAndromeda <andromeda4869game@gmail.com>
2025-01-06 22:51:02 +0900

    update

--- LoxSharp/Scanner.cs
+++ LoxSharp/Scanner.cs
@@ -3,9 +3,18 @@ namespace LoxSharp;
 /// <summary>
 /// スクリプトをトークンに分解する字句解析器 (scanning, lexing)
 /// </summary>
-public class Scanner(string source)
+public class Scanner
 {
-	private readonly string _source = source;
+	private readonly string _source;
+
+	private int start = 0;
+	private int current = 0;
+	private int line = 1;
+
+	public Scanner(string source)
+	{
+		_source = source;
+	}
 
 	/// <summary>
 	/// トークンに分解する
@@ -13,6 +22,14 @@ public class Scanner(string source)
 	public List<Token> ScanTokens()
 	{
 		var tokens = new List<Token>();
+		while (!IsAtEnd())
+		{
+		}
 		return tokens;
+	}
+
+	private bool IsAtEnd()
+	{
+		return current >= _source.Length;
 	}
 }
--- LoxSharp/Token.cs
+++ LoxSharp/Token.cs
@@ -2,4 +2,21 @@ namespace LoxSharp;
 
 public class Token
 {
+    private TokenType Type { get; init; }
+    private string Lexeme { get; init; }
+    public object Literal { get; init; }
+    public int Line { get; init; }
+
+    public Token(TokenType type, string lexeme, object literal, int line)
+    {
+        Type = type;
+        Lexeme = lexeme;
+        Literal = literal;
+        Line = line;
+    }
+    
+    public override string ToString()
+    {
+        return $"{Type} {Lexeme} {Literal}";
+    }
 }
